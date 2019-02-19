bceed371d39a06cf8b23f6acaf0a89daab8b1ad7
TPei <t.s.peikert@gmail.com>
2019-02-19 13:47:02 +0100

    fix swift anonymizer return data

--- functions/swift_anonymizer/anonymizer.swift
+++ functions/swift_anonymizer/anonymizer.swift
@@ -2,7 +2,7 @@ func main(args: [String:Any]) -> [String:Any] {
     if let data = args["data"] as? String {
         let length = data.count
         let response = String(repeating: "*", count: length)
-        return [ "greeting" : response ]
+        return [ "data" : response ]
     } else {
         return [ "data" : "" ]
     }
